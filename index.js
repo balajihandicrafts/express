91d9874e9c08e90c07775d37df9c9ef78e365d9a
balajihandicrafts <159001683+balajihandicrafts@users.noreply.github.com>
2024-02-05 14:02:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const path = require("path");
 
 
 const app = express();
-const PORT = 3001;
-const imageUrl = "http://localhost:3001/";
+const imageUrl = "https://minifiedecomm.onrender.com/";
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
@@ -124,5 +123,5 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/banners", express.static(path.join(__dirname, "banners")));  
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port `);
 });
